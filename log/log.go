8aba0c69c81549e536a639d05888a1c5f81904a8
Kyle91 <824207896@qq.com>
2024-06-08 23:13:01 +0800

    feat：修改日志库

--- log/log.go
+++ log/log.go
@@ -1,97 +1,153 @@
 // @Author Eric
 // @Date 2024/6/2 17:36:00
-// @Desc 在 logrus 的基础上进行二次封装
+// @Desc 日志库，支持并发，自动轮替
 package log
 
 import (
-	"github.com/sirupsen/logrus"
-	"gopkg.in/natefinch/lumberjack.v2"
+	"fmt"
 	"os"
 	"path/filepath"
 	"runtime"
+	"strings"
+	"sync"
+	"time"
 )
 
-type Log struct {
-	*logrus.Logger
+// Logger struct holds the logger configuration and state
+type Logger struct {
+	sync.Mutex
+	file        *os.File
 	serviceName string
+	maxSize     int64
+	maxBackups  int
+	logDir      string
+	currSize    int64
 }
 
-// NewLogger 创建一个新的 Logger 实例,默认日志级别为 Info
-func NewLogger(serviceName string) *Log {
-	logger := logrus.New()
+var logger *Logger
 
-	homeDir, err := os.UserHomeDir()
-	if err != nil {
-		logrus.Fatal(err)
+// Init initializes the logger
+func Init(serviceName, logDir string, maxSize int64, maxBackups int) error {
+	if err := os.MkdirAll(logDir, os.ModePerm); err != nil {
+		return fmt.Errorf("failed to create log directory: %w", err)
 	}
 
-	logDir := filepath.Join(homeDir, "haven", "log")
-	err = os.MkdirAll(logDir, os.ModePerm)
+	logFile := filepath.Join(logDir, "logfile.log")
+	file, err := os.OpenFile(logFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
-		logrus.Fatal(err)
+		return fmt.Errorf("failed to open log file: %w", err)
 	}
 
-	logPath := filepath.Join(logDir, "haven.log")
-	file, err := os.OpenFile(logPath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
-	if err != nil {
-		logrus.Fatal(err)
-	}
-	defer file.Close()
-
-	logger.SetOutput(os.Stdout)
-	logger.SetFormatter(&logrus.TextFormatter{})
-	logger.SetLevel(logrus.InfoLevel)
-
-	logrus.SetOutput(&lumberjack.Logger{
-		Filename:   logPath, // 日志文件的位置
-		MaxSize:    20,      // 兆为单位
-		MaxBackups: 10,      //最大备份数量
-		MaxAge:     28,      //最大保存时间（以天为单位）
-		Compress:   true,    // 是否压缩日志
-	})
-
-	return &Log{
-		Logger:      logger,
+	logger = &Logger{
+		file:        file,
 		serviceName: serviceName,
+		maxSize:     maxSize,
+		maxBackups:  maxBackups,
+		logDir:      logDir,
 	}
-}
 
-// WithContext 添加上下文信息到日志条目中
-func (l *Log) WithContext() *logrus.Entry {
-	pc, file, line, ok := runtime.Caller(2)
-	var fnName string
-	if ok {
-		fnName = runtime.FuncForPC(pc).Name()
+	// Get the initial size of the log file
+	stat, err := file.Stat()
+	if err != nil {
+		return fmt.Errorf("failed to stat log file: %w", err)
 	}
-	return l.WithFields(logrus.Fields{
-		"service": l.serviceName,
-		"file":    file,
-		"line":    line,
-		"func":    fnName,
-	})
+	logger.currSize = stat.Size()
+
+	return nil
 }
 
-// Infof 包装 logrus 的 Infof 方法，添加上下文信息
-func (l *Log) Infof(format string, args ...interface{}) {
-	l.WithContext().Infof(format, args...)
+// log logs the message with the specified level
+func (l *Logger) log(level, msg string) {
+	l.Lock()
+	defer l.Unlock()
+
+	// Check if we need to rotate the log
+	if l.currSize >= l.maxSize {
+		l.rotateLogs()
+	}
+
+	// Prepare the log entry
+	entry := fmt.Sprintf("%s [%s] %s %s:%d %s\n",
+		time.Now().Format(time.RFC3339Nano), level, l.serviceName, getFuncName(), getLine(), msg)
+
+	// Write to the log file
+	n, err := l.file.WriteString(entry)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to write to log file: %v\n", err)
+	}
+
+	// Write to the console
+	fmt.Print(entry)
+
+	l.currSize += int64(n)
 }
 
-// Errorf 包装 logrus 的 Errorf 方法，添加上下文信息
-func (l *Log) Errorf(format string, args ...interface{}) {
-	l.WithContext().Errorf(format, args...)
+// rotateLogs rotates the log files
+func (l *Logger) rotateLogs() {
+	l.file.Close()
+
+	for i := l.maxBackups - 1; i >= 0; i-- {
+		oldPath := filepath.Join(l.logDir, fmt.Sprintf("logfile.log.%d", i))
+		newPath := filepath.Join(l.logDir, fmt.Sprintf("logfile.log.%d", i+1))
+		if _, err := os.Stat(oldPath); err == nil {
+			os.Rename(oldPath, newPath)
+		}
+	}
+
+	os.Rename(filepath.Join(l.logDir, "logfile.log"), filepath.Join(l.logDir, "logfile.log.0"))
+
+	file, err := os.OpenFile(filepath.Join(l.logDir, "logfile.log"), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to open new log file: %v\n", err)
+		return
+	}
+
+	l.file = file
+	l.currSize = 0
 }
 
-// Debugf 包装 logrus 的 Debugf 方法，添加上下文信息
-func (l *Log) Debugf(format string, args ...interface{}) {
-	l.WithContext().Debugf(format, args...)
+// getFuncName returns the name of the function that called the logger
+func getFuncName() string {
+	pc, _, _, ok := runtime.Caller(2)
+	if !ok {
+		return "unknown"
+	}
+	fn := runtime.FuncForPC(pc)
+	if fn == nil {
+		return "unknown"
+	}
+	parts := strings.Split(fn.Name(), "/")
+	return parts[len(parts)-1]
 }
 
-// Warningf 包装 logrus 的 Warningf 方法，添加上下文信息
-func (l *Log) Warningf(format string, args ...interface{}) {
-	l.WithContext().Warningf(format, args...)
+// getLine returns the line number where the logger was called
+func getLine() int {
+	_, _, line, ok := runtime.Caller(2)
+	if !ok {
+		return 0
+	}
+	return line
 }
 
-// Tracef 包装 logrus 的 Tracef 方法，添加上下文信息
-func (l *Log) Tracef(format string, args ...interface{}) {
-	l.WithContext().Tracef(format, args...)
+// Exported logging functions
+
+func Debug(msg string) {
+	logger.log("DEBUG", msg)
+}
+
+func Info(msg string) {
+	logger.log("INFO", msg)
+}
+
+func Warn(msg string) {
+	logger.log("WARN", msg)
+}
+
+func Error(msg string) {
+	logger.log("ERROR", msg)
+}
+
+func Fatal(msg string) {
+	logger.log("FATAL", msg)
+	os.Exit(1)
 }
