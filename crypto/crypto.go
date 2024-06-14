20b69ae3adea7022a67ff730e176f7b02878828d
Kyle91 <824207896@qq.com>
2024-06-14 12:10:22 +0800

    feat:加解密调整

--- crypto/crypto.go
+++ crypto/crypto.go
@@ -33,14 +33,14 @@ func pkcs7Unpadding(data []byte) ([]byte, error) {
 	return data[:(length - padding)], nil
 }
 
-// Aes256Encrypt
+// Aes256EncryptBase64
 //
 //	@Description: AES256加密，CBC模式，PKCS7Padding，iv就是密钥
 //	@param key 加密key
 //	@param plaintext 原始数据
 //	@return string 返回base64之后数据
 //	@return error
-func Aes256Encrypt(key []byte, plaintext string) (string, error) {
+func Aes256EncryptBase64(key []byte, plaintext string) (string, error) {
 	if len(key) != 32 {
 		return "", errors.New("key length must be 32 bytes")
 	}
@@ -58,14 +58,14 @@ func Aes256Encrypt(key []byte, plaintext string) (string, error) {
 	return base64.StdEncoding.EncodeToString(ciphertext), nil
 }
 
-// Aes256Decrypt
+// Aes256DecryptBase64
 //
 //	@Description: AES256解密,CBC模式，PKCS7Padding，iv就是密钥
 //	@param key 加密key
 //	@param ciphertext base64的密文
 //	@return []byte 解密后数据
 //	@return error
-func Aes256Decrypt(key []byte, ciphertext string) ([]byte, error) {
+func Aes256DecryptBase64(key []byte, ciphertext string) ([]byte, error) {
 	if len(key) != 32 {
 		return nil, errors.New("key length must be 32 bytes")
 	}
@@ -87,6 +87,65 @@ func Aes256Decrypt(key []byte, ciphertext string) ([]byte, error) {
 
 	mode := cipher.NewCBCDecrypter(block, key[:aes.BlockSize])
 	mode.CryptBlocks(plaintext, ciphertextBytes)
+
+	plaintext, err = pkcs7Unpadding(plaintext)
+	if err != nil {
+		return nil, err
+	}
+
+	return plaintext, nil
+}
+
+// Aes256Encrypt
+//
+//	@Description: AES256加密，CBC模式，PKCS7Padding，iv就是密钥
+//	@param key 加密key
+//	@param plaintext 原始数据
+//	@return byte 返回加密后数据
+//	@return error
+func Aes256Encrypt(key []byte, plaintext []byte) ([]byte, error) {
+	if len(key) != 32 {
+		return nil, errors.New("key length must be 32 bytes")
+	}
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	paddedData := pkcs7Padding(plaintext, aes.BlockSize)
+	ciphertext := make([]byte, len(paddedData))
+
+	mode := cipher.NewCBCEncrypter(block, key[:aes.BlockSize])
+	mode.CryptBlocks(ciphertext, paddedData)
+
+	return ciphertext, nil
+}
+
+// Aes256Decrypt
+//
+//	@Description: AES256解密,CBC模式，PKCS7Padding，iv就是密钥
+//	@param key 加密key
+//	@param ciphertext 密文
+//	@return []byte 解密后数据
+//	@return error
+func Aes256Decrypt(key []byte, cipherBytes []byte) ([]byte, error) {
+	if len(key) != 32 {
+		return nil, errors.New("key length must be 32 bytes")
+	}
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(cipherBytes) < aes.BlockSize {
+		return nil, errors.New("ciphertext too short")
+	}
+
+	plaintext := make([]byte, len(cipherBytes))
+
+	mode := cipher.NewCBCDecrypter(block, key[:aes.BlockSize])
+	mode.CryptBlocks(plaintext, cipherBytes)
 
 	plaintext, err = pkcs7Unpadding(plaintext)
 	if err != nil {
