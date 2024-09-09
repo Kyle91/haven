e5d054348e896afb791a13cdff4ede45c4132aa8
Kyle91 <824207896@qq.com>
2024-09-09 10:45:54 +0800

    fix：修复生成token bug

--- auth/authtoken.go
+++ auth/authtoken.go
@@ -4,10 +4,13 @@
 package auth
 
 import (
+	"crypto/rand"
 	"encoding/base64"
+	"encoding/hex"
 	"errors"
 	"fmt"
 	"github.com/Kyle91/haven/crypto"
+	"io"
 	"strconv"
 	"strings"
 	"time"
@@ -28,34 +31,56 @@ func NewAuthToken(secretKey, salt string) *AuthToken {
 	}
 }
 
+// 生成随机字符串
+func generateRandomString() (string, error) {
+	randBytes := make([]byte, 16) // 生成16字节的随机数
+	if _, err := io.ReadFull(rand.Reader, randBytes); err != nil {
+		return "", err
+	}
+	return hex.EncodeToString(randBytes), nil
+}
+
 // 生成 Token
 func (a *AuthToken) GenerateToken(userID int64, expirationTime int64) (string, error) {
 	// 1. 生成过期时间戳
 	expiration := time.Now().Add(time.Duration(expirationTime) * time.Second).Unix()
 
-	// 2. 拼接原始数据: userID:expiration:salt
-	rawData := fmt.Sprintf("%d:%d:%s", userID, expiration, a.Salt)
+	// 2. 生成一个随机字符串
+	randomString, err := generateRandomString()
+	if err != nil {
+		return "", err
+	}
 
-	// 3. 对原始数据进行加密，生成 token
+	// 3. 拼接原始数据: userID:expiration:salt:randomString
+	rawData := fmt.Sprintf("%d:%d:%s:%s", userID, expiration, a.Salt, randomString)
+
+	// 4. 对原始数据进行加密，生成 token
 	token, err := crypto.Aes256Encrypt(a.SecretKey, []byte(rawData))
 	if err != nil {
 		return "", err
 	}
 
-	return string(token), nil
+	// 5. 将加密后的token转换为十六进制字符串
+	return hex.EncodeToString(token), nil
 }
 
 // 解析 Token
 func (a *AuthToken) ParseToken(token string) (int64, bool, error) {
-	// 1. 对 token 进行解密，获取原始数据
-	decryptedData, err := crypto.Aes256Decrypt(a.SecretKey, []byte(token))
+	// 1. 将十六进制字符串解码为字节数组
+	tokenBytes, err := hex.DecodeString(token)
+	if err != nil {
+		return 0, false, errors.New("invalid token format")
+	}
+
+	// 2. 对 token 进行解密，获取原始数据
+	decryptedData, err := crypto.Aes256Decrypt(a.SecretKey, tokenBytes)
 	if err != nil {
 		return 0, false, err
 	}
 
-	// 2. 拆分原始数据
+	// 3. 拆分原始数据
 	parts := strings.Split(string(decryptedData), ":")
-	if len(parts) != 3 {
+	if len(parts) != 4 {
 		return 0, false, errors.New("invalid token format")
 	}
 
@@ -69,12 +94,12 @@ func (a *AuthToken) ParseToken(token string) (int64, bool, error) {
 	}
 	tokenSalt := parts[2]
 
-	// 3. 校验盐值是否匹配
+	// 4. 校验盐值是否匹配
 	if tokenSalt != a.Salt {
 		return 0, false, errors.New("salt mismatch")
 	}
 
-	// 4. 校验是否过期
+	// 5. 校验是否过期
 	currentTime := time.Now().Unix()
 	if currentTime > expiration {
 		return userID, false, errors.New("token expired")
