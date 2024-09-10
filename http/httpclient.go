3b0708cdbffcdb606fc5fe07f6d0455520fb2af9
Kyle91 <824207896@qq.com>
2024-09-10 23:58:51 +0800

    Update httpclient.go

--- http/httpclient.go
+++ http/httpclient.go
@@ -34,3 +34,28 @@ func PostRequest(url, userAgent string, body []byte) ([]byte, error) {
 	// 返回响应Body
 	return resp.Body(), nil
 }
+
+func GetRequest(url, userAgent string) ([]byte, error) {
+	// 创建请求和响应对象
+	req := fasthttp.AcquireRequest()
+	defer fasthttp.ReleaseRequest(req)
+	resp := fasthttp.AcquireResponse()
+	defer fasthttp.ReleaseResponse(resp)
+
+	// 设置请求方法和URL
+	req.Header.SetMethod(fasthttp.MethodGet)
+	req.SetRequestURI(url)
+
+	// 设置User-Agent
+	req.Header.Set("User-Agent", userAgent)
+
+	// 发送请求
+	client := &fasthttp.Client{}
+	err := client.Do(req, resp)
+	if err != nil {
+		return nil, fmt.Errorf("发送请求时出错: %v", err)
+	}
+
+	// 返回响应Body
+	return resp.Body(), nil
+}
